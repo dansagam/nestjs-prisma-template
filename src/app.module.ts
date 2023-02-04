fb1fdfb43c0acb3306c14d8ff24e1f160fd5e4ee
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2023-02-04 12:07:11 +0100

    install: the nestjs config for the utilisation of te dotenv in nest application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelpModule } from 'views/help/help.module';
 
 @Module({
-  imports: [HelpModule],
+  imports: [HelpModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 6541;
 
   const options = new DocumentBuilder()
     .setTitle('Nest BG API Swagger')
@@ -17,7 +20,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(6541);
+  await app.listen(port);
 
   console.log(`Application is running on ${await app.getUrl()}`);
 }
