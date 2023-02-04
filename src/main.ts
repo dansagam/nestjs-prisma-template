ae71a456126de3421e95285f3959318ac4c3cca3
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2023-02-04 11:00:13 +0100

    feat: set up the swagger features

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest BG API Swagger')
+    .setDescription('The NEst BG API description')
+    .setVersion('1.0')
+    .addTag('change')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(6541);
+
+  console.log(`Application is running on ${await app.getUrl()}`);
 }
 bootstrap();
